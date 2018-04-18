f9be34c182398f2dbad7c51d09204c9af5e059f5
shubhamarora2k11 <shubhamarora2k11@users.noreply.github.com>
2018-04-18 12:24:16 +0530

    Update RegexSubstituteOnACsvColumn.py
    Added instructions in comments

--- RegexSubstituteOnACsvColumn.py
+++ RegexSubstituteOnACsvColumn.py
@@ -1,10 +1,14 @@
+#Replace COLUMN_NAME with your column
+#Replace FILENAME with your csv filename
+#Replace REGEX with your requirements like (\w+(\-||)\w+\.\w+\.[a-z0-9.]*)
+
 import pandas as pd
 import re
 
 def replace_main(dataframe1):
     dataframe2=dataframe1
     i=0
-    for row in dataframe1['COLUMN_NAME']:
+    for row in dataframe1['COLUMN_NAME']: 
              replaced = re.sub('REGEX', 'REPLACE_WITH', row) 
              dataframe2['COLUMN_NAME'][i]= replaced
              i+=1
