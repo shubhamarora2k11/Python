e2d5520d324e6d9f16af7ddf37b338d33a5d5d5d
shubhamarora2k11 <shubhamarora2k11@users.noreply.github.com>
2018-04-18 12:15:09 +0530

    Create RegexSubstituteOnACsvColumn.py

---
+++ RegexSubstituteOnACsvColumn.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import re
+
+def replace_main(dataframe1):
+    dataframe2=dataframe1
+    i=0
+    for row in dataframe1['COLUMN_NAME']:
+             replaced = re.sub('REGEX', 'REPLACE_WITH', row) 
+             dataframe2['COLUMN_NAME'][i]= replaced
+             i+=1
+    return dataframe
+
+def main():
+    dataframe1 = pd.read_csv("FILE_NAME.csv") 
+    dataframe2= replace_main(dataframe1)
+    print(dataframe2['COLUMN_NAME'])
+
+main()
+
